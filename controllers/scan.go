a5e5812e6b62f96bd3daf2683b5a265074cca6b1
fanbc <bch.fan@hnair.com>
2018-01-26 15:04:11 +0800

    删除os api

--- controllers/scan.go
+++ controllers/scan.go
@@ -11,7 +11,6 @@ import (
 	"github.com/zhanglianx111/clair-plus/client"
 	"github.com/zhanglianx111/clair-plus/models"
 	"github.com/zhanglianx111/clair-plus/mq"
-	"github.com/zhanglianx111/clair-plus/system"
 )
 
 type ScanController struct {
