41779551bcd2ef9767157d811f3592233dcdc46b
fanbc <bch.fan@hnair.com>
2018-01-30 12:54:20 +0800

    tesg

--- mq/redis.go
+++ mq/redis.go
@@ -119,7 +119,7 @@ func sendResult(sendStr sendStruct, image models.Image) {
 	sendURL :=  webUrl + "/v1/clair/" + "registry/hub.hcpaas.com/namespace/" + namespace + "/image/" + imageName + "/tag/" + image.Tag + "/imageReport"
 
 	req := httplib.Put(sendURL)
-	req.JSONBody(&sendStr)
+	req.JSONBody(sendStr)
 	req.Header("Content-Type", "application/json;charset=utf-8")
 
 	resp, err := req.DoRequest()
